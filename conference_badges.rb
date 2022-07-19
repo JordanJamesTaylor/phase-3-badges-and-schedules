a6b5c5a9c563fd61eabefbb613286a339211264f
jordan <jordjamestaylor@gmail.com>
2022-07-18 20:29:12 -0400

    commit tree 938dca5ce2cf129b90e2b72005ab1ac7e5e42bf8

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,21 @@
 # Write your code here.
+def badge_maker(name)
+    "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(array)
+    array.map do |person|
+        "Hello, my name is #{person}."
+    end
+end
+
+def assign_rooms(array)
+    pp batch_badge_creator(array)
+    array.each_with_index.map do | person, index |
+        "Hello, #{person}! You'll be assigned to room #{index + 1}!"
+    end
+end
+
+def printer(array)
+    pp assign_rooms(array)
+end
